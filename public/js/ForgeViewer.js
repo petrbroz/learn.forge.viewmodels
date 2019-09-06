3163e6aa1b9455c4f27fc97772906b2ce0ccc875
Petr Broz <ipetrbroz@gmail.com>
2019-09-06 17:05:08 +0200

    Try reusing viewer when switching models...
    ... by keeping the viewer instance and only calling
    `unloadDocumentNode` and `loadDocumentNode`.

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -16,27 +16,33 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
 
-var viewer;
+let viewer;
+let viewable;
 
 function launchViewer(urn) {
-  var options = {
-    env: 'AutodeskProduction',
-    getAccessToken: getForgeToken
-  };
-
-  Autodesk.Viewing.Initializer(options, () => {
-    viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'));
-    viewer.start();
-    var documentId = 'urn:' + urn;
+  if (!viewer) {
+    const options = {
+      env: 'AutodeskProduction',
+      getAccessToken: getForgeToken
+    };
+    Autodesk.Viewing.Initializer(options, () => {
+      viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'));
+      viewer.start();
+      const documentId = 'urn:' + urn;
+      Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+    });
+  } else {
+    const documentId = 'urn:' + urn;
     Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-  });
+  }
 }
 
 function onDocumentLoadSuccess(doc) {
-  var viewables = doc.getRoot().getDefaultGeometry();
-  viewer.loadDocumentNode(doc, viewables).then(i => {
-    // documented loaded, any action?
-  });
+  if (viewable) {
+    viewer.unloadDocumentNode(viewable);
+  }
+  viewable = doc.getRoot().getDefaultGeometry();
+  viewer.loadDocumentNode(doc, viewable);
 }
 
 function onDocumentLoadFailure(viewerErrorCode) {
