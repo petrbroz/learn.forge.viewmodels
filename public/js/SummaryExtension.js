87905a4cd5392cd1032c67aacaac6e89b1a7a7da
Petr Broz <ipetrbroz@gmail.com>
2019-09-06 12:27:21 +0200

    Update summary panel on selection change.

--- public/js/SummaryExtension.js
+++ public/js/SummaryExtension.js
@@ -5,6 +5,8 @@ class SummaryExtension extends Autodesk.Viewing.Extension {
 
     load() {
         this._panel = new SummaryPanel(this.viewer.container, 'summaryPanel', 'Summary Panel');
+        this.viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT,
+            this.onSelectionChanged.bind(this));
         console.log('SummaryExtension loaded.');
         return true;
     }
@@ -28,6 +30,33 @@ class SummaryExtension extends Autodesk.Viewing.Extension {
         this._button.setToolTip('Summary');
         this._button.addClass('summaryButtonIcon');
         this._group.addControl(this._button);
+    }
+
+    onSelectionChanged() {
+        const selectedIds = this.viewer.getSelection();
+        if (selectedIds.length > 0) {
+            const filterProps = ['Area', 'Volume'];
+            this.viewer.model.getBulkProperties(selectedIds, filterProps, (items) => {
+                let totalArea = 0.0, totalVolume = 0.0;
+                for (const item of items) {
+                    const areaProp = item.properties.find(prop => prop.displayName === 'Area');
+                    if (areaProp) {
+                        totalArea += parseFloat(areaProp.displayValue);
+                    }
+                    const volumeProp = item.properties.find(prop => prop.displayName === 'Volume');
+                    if (volumeProp) {
+                        totalVolume += parseFloat(volumeProp.displayValue);
+                    }
+                }
+                this._panel.removeAllProperties();
+                this._panel.addProperty('Total Area', totalArea.toFixed(2), 'Measurements');
+                this._panel.addProperty('Total Volume', totalVolume.toFixed(2), 'Measurements');
+            });
+        } else {
+            this._panel.removeAllProperties();
+            this._panel.addProperty('Total Area', '0.00', 'Measurements');
+            this._panel.addProperty('Total Volume', '0.00', 'Measurements');
+        }
     }
 }
 
